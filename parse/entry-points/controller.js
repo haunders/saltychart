af40a9a525e74b312f71b30011587f93cfa0054e
haunders <haunders@github.com>
2026-01-03 12:15:48 +0500

    Error 500

--- parse/entry-points/controller.js
+++ parse/entry-points/controller.js
@@ -3,25 +3,31 @@ import { parsePosts } from "../vk-requests.js";
 
 export const ParseController = new class {
     getParse = async function (req, res) {
-        const post = parseInt(req.query['post']);
-        let max_date_next = new Date(await getChartMaxDate());
-        max_date_next.setDate(max_date_next.getDate() + 1);
-        const date_str = max_date_next.toISOString().split('T')[0];
+        try {
+            const post = parseInt(req.query['post']);
+            let max_date_next = new Date(await getChartMaxDate());
+            max_date_next.setDate(max_date_next.getDate() + 1);
+            const date_str = max_date_next.toISOString().split('T')[0];
 
-        if (post === null || isNaN(post)) {
-            return res.render('parse', {
-                values: [],
-                date: date_str,
-                post: 0
-            });
+            if (post === null || isNaN(post)) {
+                return res.render('parse', {
+                    values: [],
+                    date: date_str,
+                    post: 0
+                });
+            }
+            
+            try {
+                const values = await parsePosts(post);
+                res.render('parse', {
+                    values: values,
+                    date: date_str,
+                    post: post
+                });
+            }
+            catch (e) { res.render('404'); }
         }
-        
-        const values = await parsePosts(post);
-        res.render('parse', {
-            values: values,
-            date: date_str,
-            post: post
-        });
+        catch (e) { res.render('500'); }
     };
 };
 
